741815a5017e5529098c00fe8e455bb176fb2582
VSH Kiran <saiharikiran@outlook.com>
2024-03-04 13:32:29 +0530

    tenth commit

--- app/api/mail/route.js
+++ app/api/mail/route.js
@@ -4,18 +4,18 @@ export async function POST(request) {
   const data = await request.json();
   console.log(data);
   const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
+    host: "smtp-mail.outlook.com",
     port: 587,
     secure: false,
     auth: {
-      user: "saisurya.teja@outlook.com",
-      pass: "qspqphrpkhycirty",
+      user: "kirankittu3760@gmail.com",
+      pass: "cknxcjaatzxbrtzf",
     },
   });
   // Compose email
   const mailOptions = {
-    from: "saisurya.teja@outlook.com",
-    to: "saisurya.teja@outlook.com",
+    from: "kirankittu3760@gmail.com",
+    to: "kirankittu3760@gmail.com",
     subject: "New Form Submission",
     text: `Name:${data.name}, Email: ${data.mail} Type of service: ${data.service}`,
   };
